925184039bb0d121b378d056fe5f3bf409c68ac8
Josh <jw48152@gmail.com>
2022-09-17 14:22:52 -0400

    Fixed line spacing

--- Src/main.c
+++ Src/main.c
@@ -31,9 +31,9 @@ void ledOn(uint32_t* pGPIO_D_ModeReg, uint32_t* pGPIO_D_OutData);
 int main(void)
 {
 	printf("Enable led on PD12");
-	uint32_t *pClockCtrlReg 			= (uint32_t*)0x40023830;
+	uint32_t *pClockCtrlReg			= (uint32_t*)0x40023830;
 	uint32_t *pGpiodModeReg 		= (uint32_t*)0x40020C00;
-	uint32_t *pGpiodOutputDataReg 	= (uint32_t*)0x40020C14;
+	uint32_t *pGpiodOutputDataReg	= (uint32_t*)0x40020C14;
 
 	// enable the clock for GPIOD peripheral in the AHB1ENR
 	*pClockCtrlReg |= (1 << 3);
@@ -62,14 +62,14 @@ void delayFunc(void) {
 
 void ledOff(uint32_t* pGPIO_D_OutData, uint32_t* pGPIO_D_ModeReg) {
 	// turn off led
-	*pGPIO_D_OutData 	&= 0xFFFFFFFF;
-	*pGPIO_D_ModeReg 	&= 0x0000;
+	*pGPIO_D_OutData	&= 0xFFFFFFFF;
+	*pGPIO_D_ModeReg	&= 0x0000;
 }
 
 void ledOn(uint32_t* pGPIO_D_ModeReg, uint32_t* pGPIO_D_OutData) {
 	// clear the bits using mask
-	*pGPIO_D_ModeReg &= ~(3 << 10);
-	*pGPIO_D_ModeReg |= (1 << 10);
+	*pGPIO_D_ModeReg	&= ~(3 << 10);
+	*pGPIO_D_ModeReg	|= (1 << 10);
 	// set the 5th pin
-	*pGPIO_D_OutData |= (1 << 5);
+	*pGPIO_D_OutData	|= (1 << 5);
 }
